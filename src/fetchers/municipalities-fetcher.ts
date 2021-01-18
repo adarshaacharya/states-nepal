f65a9480654c873688aed16ffacec54901db68cb
Aadarsha <connectwithadarsha@gmail.com>
2021-01-19 00:41:44 +0545

    feat: fetchers completed

--- src/fetchers/municipalities-fetcher.ts
+++ src/fetchers/municipalities-fetcher.ts
@@ -4,11 +4,11 @@ import fs from 'fs'
 
 type Language = 'en' | 'np'
 
-function fetchDistricts(lang: Language = 'en') {
+function fetchMunicipalities(lang: Language = 'en') {
 	const file = lang === 'np' ? 'np.json' : 'en.json'
-	const categories = fs.readFileSync(`data/districts/${file}`, 'utf-8')
+	const categories = fs.readFileSync(`data/municipalities/${file}`, 'utf-8')
 	const json = JSON.parse(categories)
 	return json
 }
 
-export default fetchDistricts
+export default fetchMunicipalities
