cea0f77883547d420f8c9fccd20a43d0accac863
Aadarsha <connectwithadarsha@gmail.com>
2021-01-25 13:52:58 +0545

    feat: add categories methods function

---
+++ src/models/category.ts
@@ -0,0 +1,64 @@
+import fetchCategory from '../fetchers/categories-fetcher'
+type Language = 'en' | 'np'
+type Key = 'id' | 'shortCode' | 'name'
+
+interface ICategories {
+	id: number
+	name: string
+	shortCode: string
+}
+
+class Category {
+	private categories: ICategories[]
+	private lang
+
+	constructor(lang: Language = 'en') {
+		try {
+			this.lang = lang
+			this.categories = fetchCategory(this.lang)
+		} catch (e) {
+			throw new Error(e)
+		}
+	}
+
+	/**
+	 * Get list of all categories
+	 *
+	 * @returns categories array
+	 */
+	public allCategories() {
+		return this.categories
+	}
+
+	/**
+	 * Find category deatils by unique id
+	 *
+	 * @returns category | null
+	 */
+	public find(id: number) {
+		const category = this.categories.find(el => el.id === id)
+		return category ? category : null
+	}
+
+	/**
+	 * Find category details by short code
+	 *
+	 * @param shortCode
+	 * @return category|null
+	 */
+	public findByShortCode(shortCode: string) {
+		const category = this.categories.find(el => el.shortCode === shortCode)
+		return category ? category : null
+	}
+
+	/**
+	 * Search Categories
+	 *
+	 * @param key
+	 * @param value
+	 * @return array of categories that match with given key
+	 */
+	public search(key: Key, value: string) {
+		return this.categories.filter(el => el[key] === value)
+	}
+}
---
+++ src/utils/converter.ts
@@ -0,0 +1,15 @@
+const nepali = ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९']
+
+const english = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+/**
+ * @description: Convert english numeric value to nepali numeric value
+ *
+ * @param value
+ * @returns mixed
+ */
+function numericNepali(value : string) {
+    for (let index = 0; index < 10; index++) {
+                
+    }
+}
