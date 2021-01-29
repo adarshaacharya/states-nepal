8ab2b0bd2a86ad07573d9d5344c841d21e9aeb94
Aadarsha <connectwithadarsha@gmail.com>
2021-01-29 21:12:27 +0545

    docs: add provinces data docs

--- src/fetchers/index.ts
+++ src/fetchers/index.ts
@@ -1,14 +1,14 @@
-'use strict'
-
 import fs from 'fs'
 
 type Language = 'en' | 'np'
 type Directory = 'districts' | 'provinces' | 'categories' | 'municipalities'
 
-export function fetcher(dir : Directory,lang: Language = 'en') {
-	const file = lang === 'np' ? 'np.json' : 'en.json'
-	const content = fs.readFileSync(`data/${dir}/${file}`, 'utf-8')
-	const json = JSON.parse(content)
-	return json
+export function fetcher(dir: Directory, lang: Language = 'en') {
+	try {
+		const file = lang === 'np' ? 'np.json' : 'en.json'
+		const content = fs.readFileSync(`data/${dir}/${file}`, 'utf-8')
+		return JSON.parse(content)
+	} catch (error) {
+		throw new Error(`Failed to load  ${dir} from source`)
+	}
 }
-
