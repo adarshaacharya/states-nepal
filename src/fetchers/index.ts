e7a0cd4a594c88dda35b377741d0ee8988a55027
Aadarsha <connectwithadarsha@gmail.com>
2021-02-02 00:10:11 +0545

    fix: file path

--- src/fetchers/index.ts
+++ src/fetchers/index.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import path from 'path'
 
 type Language = 'en' | 'np'
 type Directory = 'districts' | 'provinces' | 'categories' | 'municipalities'
@@ -6,9 +7,10 @@ type Directory = 'districts' | 'provinces' | 'categories' | 'municipalities'
 export function fetcher(dir: Directory, lang: Language = 'en') {
 	try {
 		const file = lang === 'np' ? 'np.json' : 'en.json'
-		const content = fs.readFileSync(`data/${dir}/${file}`, 'utf-8')
+		const jsonPath = path.join(__dirname, `../../data/${dir}/${file}`)
+		const content = fs.readFileSync(jsonPath, 'utf-8')
 		return JSON.parse(content)
 	} catch (error) {
-		throw new Error(`Failed to load  ${dir} from source`)
+		throw new Error(`Failed to load  data from source`)
 	}
 }
